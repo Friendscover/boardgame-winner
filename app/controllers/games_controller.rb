a3b8d363196c2ab27f3eb3496d4f849bb8135ca4
Tobias Klöpper <tobiaskloepper1@web.de>
2023-01-15 11:02:55 +0100

    added a new game page to track the winner

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,24 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+    def game_params
+      params.require(:game).permit(:title, :winner)
+    end
 end
