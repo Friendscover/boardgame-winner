fca4d3b3b4e075b041637ef25c9ddef529a1badc
Tobias Klöpper <tobiaskloepper1@web.de>
2024-02-08 21:36:23 +0100

    added different colors to the game display

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,9 +2,9 @@ class GamesController < ApplicationController
   def index
     @games = Game.order(created_at: :desc).limit(5)
 
-    @games_toby = Game.where("winner = ?", "Tobias")
-    @games_teresa = Game.where("winner = ?", "Teresa")
-    @games_draw = Game.where("winner = ?", "Draw")
+    @games_toby = Game.where("winner = ?", "Tobias").order(created_at: :desc).limit(11)
+    @games_teresa = Game.where("winner = ?", "Teresa").order(created_at: :desc).limit(11)
+    @games_draw = Game.where("winner = ?", "Draw").order(created_at: :desc).limit(11)
   end
 
   def new
